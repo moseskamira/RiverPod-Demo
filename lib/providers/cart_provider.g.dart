7bb435a1599afdce426aee239da1660fa09eb50e
moseskamira <moses.african@gmail.com>
2024-08-02 10:03:49 +0300

    Setting up Riverpod

---
+++ lib/providers/cart_provider.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$cartTotalHash() => r'91f00f5a2f3659cca0ea9ec5c4dd6518104d4885';
+
+/// See also [cartTotal].
+@ProviderFor(cartTotal)
+final cartTotalProvider = AutoDisposeProvider<double>.internal(
+  cartTotal,
+  name: r'cartTotalProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cartTotalHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CartTotalRef = AutoDisposeProviderRef<double>;
+String _$cartNotifierHash() => r'0a64f37302b5b89b7b8789a0a096775c506c5495';
+
+/// See also [CartNotifier].
+@ProviderFor(CartNotifier)
+final cartNotifierProvider =
+    AutoDisposeNotifierProvider<CartNotifier, Set<ProductModel>>.internal(
+  CartNotifier.new,
+  name: r'cartNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cartNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CartNotifier = AutoDisposeNotifier<Set<ProductModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/providers/product_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productsHash() => r'925675510dea729b84eb13b925ac81019d777cc8';
+
+/// See also [products].
+@ProviderFor(products)
+final productsProvider = AutoDisposeFutureProvider<List>.internal(
+  products,
+  name: r'productsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$productsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProductsRef = AutoDisposeFutureProviderRef<List>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
